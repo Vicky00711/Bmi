390812f9052ca5738ec2183e7576e86c3fba5564
Vicky00711 <122237544+Vicky00711@users.noreply.github.com>
2023-03-08 23:32:28 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function finalResult(){
+    let height1= parseInt(document.getElementById('myHeight').value);
+    let weight1= parseInt(document.getElementById('myWeight').value);
+document.getElementById('myResult').value=weight1/height1;
+
+}
+
